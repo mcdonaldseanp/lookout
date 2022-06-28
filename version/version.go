da67b5d12a03924b347dfea3de38ef130d73cf09
Sean P. McDonald <sean.mcdonald@puppet.com>
2022-06-27 19:02:08 -0500

    (maint) Somehting?

--- version/version.go
+++ version/version.go
@@ -1,3 +1,9 @@
 package version
 
+import "fmt"
+
 const VERSION string = "v0.0.1"
+
+func ReleaseArtifact(name string) string {
+	return fmt.Sprintf("https://github.com/mcdonaldseanp/charlie/releases/download/%s/%s", VERSION, name)
+}
