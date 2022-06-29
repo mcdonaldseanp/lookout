448084c2a1b3198e0be1ee14963aa4133361a945
Sean P. McDonald <sean.mcdonald@puppet.com>
2022-06-29 13:16:29 -0500

    (release) Update to new version v0.0.2

--- version/version.go
+++ version/version.go
@@ -2,7 +2,7 @@ package version
 
 import "fmt"
 
-const VERSION string = "v0.0.1"
+const VERSION string = "v0.0.2"
 
 func ReleaseArtifact(name string) string {
 	return fmt.Sprintf("https://github.com/mcdonaldseanp/lookout/releases/download/%s/%s", VERSION, name)
