11c543d8eb4796d6b51b777ffc133cee6dcc65a8
Sean P. McDonald <sean.mcdonald@puppet.com>
2022-06-30 16:14:38 -0500

    (maint) Move over localexec functionality from charlie

--- localexec/exec_utils.go
+++ localexec/exec_utils.go
@@ -5,21 +5,30 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"runtime"
+	"strings"
 
 	"github.com/mcdonaldseanp/clibuild/errtype"
 	"github.com/mcdonaldseanp/lookout/localdata"
 	"github.com/mcdonaldseanp/lookout/sanitize"
 )
 
-func ExecReadOutput(executable string, args []string) (string, string, error) {
-	shell_command := exec.Command(executable, args...)
+func ExecReadOutput(command_string string, args ...string) (string, string, error) {
+	if runtime.GOOS == "linux" && isWinPath(command_string) {
+		translated_cmd, err := wslPathConvert(command_string)
+		if err != nil {
+			return "", "", err
+		}
+		command_string = translated_cmd
+	}
+	shell_command := exec.Command(command_string, args...)
 	shell_command.Env = os.Environ()
 	var stdout, stderr bytes.Buffer
 	shell_command.Stdout = &stdout
 	shell_command.Stderr = &stderr
 	err := shell_command.Run()
-	output := sanitize.ReplaceAllNewlines(stdout.String())
-	logs := sanitize.ReplaceAllNewlines(stderr.String())
+	output := stdout.String()
+	logs := stderr.String()
 	if err != nil {
 		return output, logs, &errtype.ShellError{
 			Message: fmt.Sprintf("Command '%s' failed:\n%s\nstderr:\n%s", shell_command, err, logs),
@@ -32,13 +41,13 @@ func ExecReadOutput(executable string, args []string) (string, string, error) {
 func ExecScriptReadOutput(executable string, script string, args []string) (string, string, error) {
 	f, err := os.CreateTemp("", "lookout_script")
 	if err != nil {
-		return "", "", fmt.Errorf("Could not create tmp file!")
+		return "", "", fmt.Errorf("could not create tmp file")
 	}
 	filename := f.Name()
 	defer os.Remove(filename) // clean up
 	localdata.OverwriteFile(filename, []byte(script))
 	final_args := append([]string{filename}, args...)
-	return ExecReadOutput(executable, final_args)
+	return ExecReadOutput(executable, final_args...)
 }
 
 func BuildAndRunCommand(executable string, file string, script string, args []string) (string, string, error) {
@@ -46,15 +55,76 @@ func BuildAndRunCommand(executable string, file string, script string, args []st
 	var err error
 	if len(file) > 0 {
 		final_args := append([]string{file}, args...)
-		output, logs, err = ExecReadOutput(executable, final_args)
+		output, logs, err = ExecReadOutput(executable, final_args...)
 	} else if len(script) > 0 {
 		output, logs, err = ExecScriptReadOutput(executable, script, args)
 	} else {
-		output, logs, err = ExecReadOutput(executable, args)
+		output, logs, err = ExecReadOutput(executable, args...)
 	}
 	if err != nil {
 		return output, logs, err
 	}
 
 	return output, logs, nil
+}
+
+// ExecAsShell always writes everything to stderr so that
+// any resulting functionality can return something useful
+// to the CLI
+func ExecAsShell(command_string string, args ...string) error {
+	if runtime.GOOS == "linux" && isWinPath(command_string) {
+		translated_cmd, err := wslPathConvert(command_string)
+		if err != nil {
+			return err
+		}
+		command_string = translated_cmd
+	}
+	shell_command := exec.Command(command_string, args...)
+	shell_command.Env = os.Environ()
+	shell_command.Stdout = os.Stderr
+	shell_command.Stderr = os.Stderr
+	shell_command.Stdin = os.Stdin
+	err := shell_command.Run()
+	if err != nil {
+		return &errtype.ShellError{
+			Message: fmt.Sprintf("command '%s' failed:\n%s", shell_command, err),
+			Origin:  err,
+		}
+	}
+	return nil
+}
+
+func ExecDetached(command_string string, args ...string) (*exec.Cmd, error) {
+	if runtime.GOOS == "linux" && isWinPath(command_string) {
+		translated_cmd, err := wslPathConvert(command_string)
+		if err != nil {
+			return nil, err
+		}
+		command_string = translated_cmd
+	}
+	shell_command := exec.Command(command_string, args...)
+	shell_command.Env = os.Environ()
+	err := shell_command.Start()
+	if err != nil {
+		return nil, &errtype.ShellError{
+			Message: fmt.Sprintf("Command '%s' failed to start:\n%s", shell_command, err),
+			Origin:  err,
+		}
+	}
+	return shell_command, nil
+}
+
+func wslPathConvert(command_string string) (string, error) {
+	if runtime.GOOS == "linux" && isWinPath(command_string) {
+		wsl_path, err_log, err := ExecReadOutput("wslpath", "-u", command_string)
+		if err != nil {
+			return "", fmt.Errorf("detected use of windows path on linux, failed attempt to use wslpath: %s\ntrace: %s", err, err_log)
+		}
+		command_string = sanitize.ReplaceAllNewlines(wsl_path)
+	}
+	return command_string, nil
+}
+
+func isWinPath(command_string string) bool {
+	return strings.HasPrefix(command_string, "C:\\") || strings.HasPrefix(command_string, "C:/")
 }
